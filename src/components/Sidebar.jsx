18b470a9e647822eda8f44acb761e845a43c41ba
daniyalmotenaim <daniyal.motan@madtechdigitals.com>
2025-07-15 13:47:17 -0700

    first-commit

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Link,Navlink} from 'react-router-dom';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import {SiShopware} from 'react-icons/si';
+import {MdOutlineCancel} from 'react-icons/md';
+import { links } from '../data/dummy';
+
+const Sidebar = () => {
+  const activeMenu = true;
+  return (
+    <div className='ml-3 h-screen md:overlflow-hidden overflow-auto md:hover:overflow-auto pb-10 '>
+      {activeMenu && (<>
+        <div className='flex justify-between items-center'>
+          <Link to="/"onClick={()=>{}} className='items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900'>
+          <SiShopware
+            className='text-2xl' />
+            <span>Shopware</span>
+          </Link>
+        </div>
+      </>)}
+
+    </div>
+  )
+}
+
+export default Sidebar
